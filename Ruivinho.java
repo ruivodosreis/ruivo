4cb72f00b3e90cfa4e765152dfcd7f65951c4a69
ruivodosreis <110936046+ruivodosreis@users.noreply.github.com>
2022-08-10 14:34:45 -0300

    Add files via upload

---
+++ Ruivinho.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ruivinho;
+
+/**
+ *
+ * @author Aluno
+ */
+
+    /**
+     * @param args the command line arguments
+     */
+       import java.util.Scanner;
+
+       public class Ruivinho {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+
+        int c= 0;
+        System.out.println("digite: 3, 4 ou 5: ");
+        int codigo = entrada.nextInt();
+        System.out.println("Informe o valor de a e b");
+        int a = entrada.nextInt();
+        int b =entrada.nextInt();
+
+        switch (codigo) {
+            case 3:
+                c =a+b;
+        {
+            int c20 = 0;
+            System.out.println( c+(c20)/100);
+        }
+
+
+                a = a+b;
+        {
+            int a20 = 0;
+            System.out.println(a=a+(a20)/100);
+        }
+
+                break;
+    }
+    }
+    
+}
